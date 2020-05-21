4e15e6b98d5def4f3673889b0c16fd7130e83333
nhuttantran98 <nhuttantran98@gmail.com>
2020-05-22 00:25:06 +0700

    Updates

---
+++ precache-manifest.9d62dc85b9f071778accf3833929bdd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e35d5fde9953f90b39e1d98d7a2beb",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "273e5c5a750ac08bf951",
+    "url": "/reactapp/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "ebdb8dd55c35f4b79193",
+    "url": "/reactapp/static/css/main.505055c0.chunk.css"
+  },
+  {
+    "revision": "273e5c5a750ac08bf951",
+    "url": "/reactapp/static/js/2.a0639a04.chunk.js"
+  },
+  {
+    "revision": "9903a508dbbe9bf2c5845c8eec5ba7be",
+    "url": "/reactapp/static/js/2.a0639a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdb8dd55c35f4b79193",
+    "url": "/reactapp/static/js/main.d6b1ba44.chunk.js"
+  },
+  {
+    "revision": "0545c319445f40a17006",
+    "url": "/reactapp/static/js/runtime-main.82d6c0db.js"
+  },
+  {
+    "revision": "c1194249040de1d95f8d55fb6eba37c0",
+    "url": "/reactapp/static/media/bg.c1194249.jpg"
+  }
+]);
